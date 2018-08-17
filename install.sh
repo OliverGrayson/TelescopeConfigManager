bbf0b4ee353413029326de26b4dcf20dd989fe4b
OliverGrayson <jmandrake42@gmail.com>
2018-08-17 11:21:38 -1000

    installer update

--- install.sh
+++ install.sh
@@ -4,12 +4,14 @@ DIR="$(ls -1)"
 echo "${OUTPUT}"
 
 echo "Installing Telescope Config Manager"
-echo `cd AngularConfManager/frontend/AngularConfManager/ && npm install && cd $DIR`
+echo `cd AngularConfManager/frontend/AngularConfManager/ && npm install && ng build`
+echo `cd $DIR`
 
 echo "Installing python dependencies"
 for dependency in "pymongo" "bson" "flask" "flask_cors"
 do
    echo `pip3 install $dependency`
 done
+
 
 echo "Install finished"
