eb57896b8c8e068fee546f7532638e2f825bcdb4
OliverGrayson <jmandrake42@gmail.com>
2018-08-17 11:28:46 -1000

    install also builds program

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# default base_href = www.keck.hawaii.edu path
+base_href = ${1:-"/inst/PILogin/ObservingTools/ConfigManager/"}
+
 DIR="$(ls -1)"
 echo "${OUTPUT}"
 
 echo "Installing Telescope Config Manager"
-echo `cd AngularConfManager/frontend/AngularConfManager/ && npm install && ng build`
+echo `cd AngularConfManager/frontend/AngularConfManager/ && npm install && npm install -g @angular/cli`
+echo `ng build --prod --base-href=$base_href`
 echo `cd $DIR`
 
 echo "Installing python dependencies"
